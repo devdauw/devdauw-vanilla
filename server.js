1c6410a2d766df8ac1141ebad49030f2361ff265
Devdauw <github@dauw.cc>
2023-10-03 22:45:26 +0200

    First iteration of a vanillaJS website Created a node based server Sever is only configured for localhost listening Server is searching for file in the FileSystem and render the content of them based on hit url Currently working for index aka '/' and returning 404 on other routes

---
+++ server.js
@@ -0,0 +1,29 @@
+"use strict";
+import http from 'node:http';
+import * as fs from 'node:fs/promises';
+
+const servePage = async (request, response) => {
+ const url = request.url;
+
+  if (url === "/") {
+    const file = await fs.readFile("./index.html");
+    response.writeHead(200);
+    response.write(file);
+    response.end();
+  }
+
+  const file = await fs.readFile("./404.html");
+  response.writeHead(404);
+  response.write(file);
+  response.end();
+}
+
+const server = http.createServer();
+
+server.listen(8080);
+
+server.on('request', (request, response) => {
+  servePage(request, response);
+});
+
+
