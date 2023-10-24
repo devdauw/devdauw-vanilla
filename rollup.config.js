ec93f71f39534f247f591eaba2f9951015cb6299
Devdauw <10974217+devdauw@users.noreply.github.com>
2023-10-24 11:50:55 +0200

    Added rollup to bundle our app It was needed to allow using modern imports for Google MaterialUI Added google material UI so that I don't recreate a new CSS library...

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+// Import rollup plugins
+import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
+import css from "rollup-plugin-import-css";
+import { copy } from '@web/rollup-plugin-copy';
+import resolve from '@rollup/plugin-node-resolve';
+import { default as terser } from '@rollup/plugin-terser';
+import summary from 'rollup-plugin-summary';
+import path from 'node:path';
+
+export default {
+  plugins: [
+    // Entry point for application build; can specify a glob to build multiple
+    // HTML files for non-SPA app
+    html({
+      input: ['index.html', 'pages/*.html', 'components/**/*.html']
+    }),
+    css(),
+    // Resolve bare module specifiers to relative paths
+    resolve(),
+    terser({
+      ecma: 2021,
+      module: true,
+      warnings: true,
+    }),
+    // Print bundle summary
+    summary(),
+    // Optional: copy any static assets to build directory
+    copy({
+      patterns: ['images/**/*'],
+    }),
+  ],
+  output: {
+    dir: 'build',
+  },
+  preserveEntrySignatures: 'strict',
+};
